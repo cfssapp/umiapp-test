276da6585ec5a0eff4e547c48e33e2583ef5722f
cfssapp <gohweihan21@gmail.com>
2023-01-15 17:19:19 +0800

    save

--- core/settings.py
+++ core/settings.py
@@ -143,6 +143,8 @@ CORS_ORIGIN_WHITELIST = [
     "http://umiapp.pythonanywhere.com",
 ]
 
+AUTH_USER_MODEL = "users.NewUser"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
