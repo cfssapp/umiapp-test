18ab3766c3e22d3ff22e17471965b8f042392d23
cfssapp <gohweihan21@gmail.com>
2023-01-15 17:16:33 +0800

    save

--- core/settings.py
+++ core/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     "corsheaders",
     
+    "users",
     "crud_app",
     "apps.audits",
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,96 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .serializers import CustomUserSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+from rest_framework import generics, permissions
+from users.models import NewUser
+from users.serializers import CustomUserSerializer
+
+from rest_framework.permissions import IsAuthenticated
+
+class CustomUserCreate(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request, format='json'):
+        serializer = CustomUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                json = serializer.data
+                return Response(json, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BlacklistTokenUpdateView(APIView):
+    permission_classes = [IsAuthenticated]
+    # authentication_classes = (IsAuthenticated)
+
+    def post(self, request, *args, **kwargs):
+        # try:
+            # refresh_token = request.data["refresh"]
+        #     refresh_token = request.data.get('refresh')
+        #     token = RefreshToken(refresh_token)
+        #     token.blacklist()
+        #     return Response(status=status.HTTP_205_RESET_CONTENT)
+        # except Exception as e:
+        #     return Response(status=status.HTTP_400_BAD_REQUEST)
+
+            refresh_token = request.data.get('refresh')
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    def validate(self, attrs):
+        data = super().validate(attrs)
+        refresh = self.get_token(self.user)
+        data['refresh'] = str(refresh)
+        data['access'] = str(refresh.access_token)
+
+        # Add extra responses here
+        data['id'] = self.user.id
+        data['user_name'] = self.user.user_name
+        data['is_active'] = self.user.is_active
+
+        data['status'] = 'ok'
+        data['currentAuthority'] = 'admin'
+        return data
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class currentUser(generics.ListAPIView):
+    queryset = NewUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+    # def get_queryset(self):
+    #     user = self.request.user
+    #     return NewUser.objects.filter(user_name=user)
+
+class currentUserDetail(generics.RetrieveAPIView):
+    queryset = NewUser.objects.all()
+    serializer_class = CustomUserSerializer 
+
+
+# TEST.MOBA.MY
+class currentUser1(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    queryset = NewUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+class currentUser1Detail(generics.RetrieveAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    queryset = NewUser.objects.all()
+    serializer_class = CustomUserSerializer 
